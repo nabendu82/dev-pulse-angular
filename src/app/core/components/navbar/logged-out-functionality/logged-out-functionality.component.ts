57e63d9079c0108069ddabe060701a90ee2ecd22
nabendu82 <nabendu.biswas@gmail.com>
2025-04-13 21:43:57 +0530

    Register done

--- src/app/core/components/navbar/logged-out-functionality/logged-out-functionality.component.ts
+++ src/app/core/components/navbar/logged-out-functionality/logged-out-functionality.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-logged-out-functionality',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './logged-out-functionality.component.html',
   styleUrl: './logged-out-functionality.component.css'
 })
